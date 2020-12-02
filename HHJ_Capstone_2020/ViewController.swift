daaba7d3943584d9835385588242f31fa713fa99
chaerinHan <chr4908@gmail.com>
2020-12-03 04:53:56 +0900

    1st commit
    Start of the project - copying the code

---
+++ HHJ_Capstone_2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HHJ_Capstone_2020
+//
+//  Created by Chaerin Han on 2020/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
