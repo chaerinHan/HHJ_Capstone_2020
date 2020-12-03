d8c72d83933e5b227a2790dd89d73fb63ccf9487
chaerinHan <chr4908@gmail.com>
2020-12-03 15:09:59 +0900

    4th commit
    add day1_repeat storyboard

---
+++ HHJ_Capstone_2020/Day1/Day1_3.0.15.0.swift
@@ -0,0 +1,36 @@
+//
+//  Day1_3.0.15.0.swift
+//  HHJ_Capstone_2020
+//
+//  Created by Chaerin Han on 2020/12/03.
+//
+
+import UIKit
+
+class Day1_3_0_15_0: UIViewController {
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBAction func audioButton(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
