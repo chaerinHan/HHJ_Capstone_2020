fe9abb2feac79f7273cd9d7e44d76019dca04589
chaerinHan <chr4908@gmail.com>
2020-12-03 12:51:25 +0900

    2nd commit
    plus storyboards and swift files

---
+++ HHJ_Capstone_2020/ChapterMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChapterMenuViewController.swift
+//  HHJ_Capstone_2020
+//
+//  Created by Chaerin Han on 2020/12/03.
+//
+
+import UIKit
+
+class ChapterMenuViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HHJ_Capstone_2020/DayMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DayMenuViewController.swift
+//  HHJ_Capstone_2020
+//
+//  Created by Chaerin Han on 2020/12/03.
+//
+
+import UIKit
+
+class DayMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
